dbd884eeeffdcca3509e620c646ccd811e992359
Mark Miranda <mmiranda@doximity.com>
2022-11-11 07:55:20 -0700

    Add render files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # root "articles#index"
+  root "pages#index"
+
   get "sign-up", to: "users#new", as: "sign_up"
   post "users", to: "users#create", as: "users"
   get "dashboard", to: "users#show", as: "dashboard"
