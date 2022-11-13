7420924d888b8552ec9e88394b5ac5100970a793
Mark Miranda <mmiranda@doximity.com>
2022-11-13 08:30:15 -0700

    User can sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    redirect_to sign_up_path unless current_user
+    redirect_to sign_in_path unless current_user
   end
 
   private
